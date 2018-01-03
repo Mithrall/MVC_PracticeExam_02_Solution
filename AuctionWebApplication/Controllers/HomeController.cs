940acbfa34bdaee4dd62850529aa1b8194362ec0
Kenneth Damkjær Nielsen <kenn6091@edu.eal.dk>
2018-01-03 16:37:56 +0100

    add connected service på bærbar

--- AuctionWebApplication/Controllers/HomeController.cs
+++ AuctionWebApplication/Controllers/HomeController.cs
@@ -1,36 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using AuctionWebApplication.Models;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
-namespace AuctionWebApplication.Controllers
-{
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
+namespace AuctionWebApplication.Controllers {
+    public class HomeController : Controller {
+        public async Task<ActionResult> Index() {
+            //var service = new AuctionService.AuctionServiceClient();
+            //List<AuctionItem> Auctions = await List<AuctionItem> GetAllAuctionItems();
             return View();
         }
 
-        public IActionResult About()
-        {
+        public IActionResult About() {
             ViewData["Message"] = "Your application description page.";
 
             return View();
         }
 
-        public IActionResult Contact()
-        {
+        public IActionResult Contact() {
             ViewData["Message"] = "Your contact page.";
 
             return View();
         }
 
-        public IActionResult Error()
-        {
+        public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
