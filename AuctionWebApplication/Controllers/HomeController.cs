fa520ea7aa5c23c1aa9e7ee54cf88872ea8f0a22
Kenneth Damkjær Nielsen <kenn6091@edu.eal.dk>
2018-01-04 01:59:53 +0100

    added view and connected service

--- AuctionWebApplication/Controllers/HomeController.cs
+++ AuctionWebApplication/Controllers/HomeController.cs
@@ -7,9 +7,9 @@ using System.Collections.Generic;
 namespace AuctionWebApplication.Controllers {
     public class HomeController : Controller {
         public async Task<ActionResult> Index() {
-            //var service = new AuctionService.AuctionServiceClient();
-            //List<AuctionItem> Auctions = await List<AuctionItem> GetAllAuctionItems();
-            return View();
+            var service = new AuctionService.AuctionsServiceClient();
+            List<AuctionService.AuctionItem> Auctions = await service.GetAllAuctionItemsAsync();
+            return View(Auctions);
         }
 
         public IActionResult About() {
